50cd06107ac5d2b597132d0ee2bc57488502761a
Josefstu <59778458+Josefstu@users.noreply.github.com>
2022-02-12 16:47:25 -0500

    Continued development
    Further progress in development, currently experiencing unresolved class reference on the _draw_bullet function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -12,10 +13,13 @@ class AlienInvasion:
         pygame.init()
 
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # Set background color
         self.bg_color = (230, 230, 230)
@@ -26,6 +30,11 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
+            self.bullets.update()
+
+            # Get rid of bullets that have disappeared
+            # for bullet in self.bullets.copy():
+            #    self.bullets.remove(bullet)
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
@@ -36,14 +45,39 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # move the ship to the right
-                    self.ship.rect.x += 1
+                self._check_keydown_events(event)
+            # Stop moving when key is released
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _fire_bullet(self):
+        # Create a new bullet and add it to the bullets group
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 
 if __name__ == '__main__':
