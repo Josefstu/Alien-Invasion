a7b57f4756361f8d7dc20efe42d01ced57144801
Josefstu <59778458+Josefstu@users.noreply.github.com>
2022-02-15 22:07:22 -0500

    Further development

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ class Settings:
 
         # Alien Settings
         self.alien_speed = 0.5
-        self.fleet_drop_speed = 50
+        self.fleet_drop_speed = 1
         # Fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
